2fd84bb2fb5fc64e998334a734ed8ea4c6095eac
JasonCharamis <jason.charamis@gmail.com>
2024-02-21 15:59:54 +0200

    Code refactoring...

--- msa_visualization/visualize_vgsc_msa.R
+++ msa_visualization/visualize_vgsc_msa.R
@@ -12,11 +12,7 @@ visualize_msa <- function(aln_filename, range, output_name="MSA") {
 
 # Call the function with the specified alignment file ("ace1.aln") and range (c(250, 300))
 l995 <- visualize_msa(aln_filename = "VGSC_orthologs.aln", range = c(1021, 1030))
-
-n1570 <- visualize_msa(aln_filename = "VGSC_orthologs.all.aln", range = c(1604, 1614))
-
-ace1 <- visualize_msa(aln_filename = "ace1.all.fasta.aln", range=c(280,310))
+n1570 <- visualize_msa(aln_filename = "VGSC_orthologs.aln", range = c(1604, 1614))
 
 ggsave(plot=l995, file="L995.svg")
 ggsave(plot=n1570, file="N1570.svg")
-ggsave(plot=ace1, file="ACE1.svg")
