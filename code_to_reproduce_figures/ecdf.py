560d50b9e9eb768f12a5d017c22913b8a520e3c1
JasonCharamis <jason.charamis@gmail.com>
2024-03-15 15:31:36 +0200

    Code refactoring...

--- code_to_reproduce_figures/ecdf.py
+++ code_to_reproduce_figures/ecdf.py
@@ -7,23 +7,36 @@ import matplotlib.pyplot as plt
 import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument('data')
+parser.add_argument('-d', '--data', type=str, help='Input tab-separated file with gene_id (column 1) and protein size (column 2).')
+parser.add_argument('-t', '--title', type = str, default = 'ECDF of provided data', help = 'Title of produced ECDF Figure.' )
+parser.add_argument('-s', '--show', action = 'store_true', default = False, help = 'Option to show the produced ECDF Figure.' )
+parser.add_argument('--show-help', action = 'help', default = False, help = 'Option to show help message.' )
 args = parser.parse_args()
 
-def ecdf(values):
-    df = pd.read_csv(values, sep="\t")
+if not any(vars(args).values()):
+    parser.print_help()
+    sys.exit('Error: No arguments provided.')
+
+
+def ecdf(values, title, show):
+    df = pd.read_csv(values, sep='\t')
     data = df.iloc[:, 1]  # Keep only the second column that contains protein sizes
 
     sorted_data = np.sort(data)
     y_values = np.arange(1, len(sorted_data) + 1) / float(len(sorted_data))
 
-    plt.rcParams['font.family'] = "arial"
+    plt.rcParams['font.family'] = 'arial'
     plt.plot(sorted_data, y_values, marker='.', linestyle='none', color='black')
     plt.xlabel('Gene expression')
     plt.ylabel('Empirical Cumulative Distribution Function')
-    plt.title('ECDF of 379 sand fly CCEs')
+    plt.title(title)
     plt.grid(False)
-    # plt.show()
-    plt.savefig('Figure_S10_379_CCEs.ecdf.svg', format="svg")
-    
-ecdf ( args.data )
+    plt.savefig( args.title+'.svg', format='svg')
+
+    if show == True:
+        plt.show()
+
+
+
+if __name__ == '__main__':    
+    ecdf (values = args.data, title = args.title, show = args.show)
